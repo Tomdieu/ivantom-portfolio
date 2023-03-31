c31a105354c0dfc931024d080997f9f3aee889a8
Tomdieu <ivantomdio@gmail.com>
2023-03-31 08:46:29 +0100

    updated

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import {Box} from '@mui/material'
+import { Box, Typography } from "@mui/material";
+import SectionDivider from "../global/SectionDivider";
 
-type Props = {}
+type Props = {};
 
 const Footer = (props: Props) => {
-    return (
-        <Box>Footer</Box>
-    )
-}
+  return (
+    <Box>
+      <SectionDivider />
+      <Box>
+        <Typography fontFamily={"Fira Code"}>Footer</Typography>
+      </Box>
+    </Box>
+  );
+};
 
-export default Footer
+export default Footer;
