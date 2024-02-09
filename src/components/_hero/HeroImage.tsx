7c48edd8aa58168308ecb0d72cf70e3d5634e6dd
Tomdieu <ivantomdio@gmail.com>
2024-02-09 21:01:11 +0100

    feat : updated hero image

--- src/components/_hero/HeroImage.tsx
+++ src/components/_hero/HeroImage.tsx
@@ -1,27 +1,27 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const HeroImage = (props: Props) => {
   return (
     <>
-    <Image
-            alt="Hero"
-            className="mx-auto aspect-video overflow-hidden dark:hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square"
-            height="550"
-            src="/logo.png"
-            width="550"
-          />
-          <Image
-            alt="Hero"
-            className="mx-auto aspect-video hidden dark:block overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square"
-            height="550"
-            src="/logo-white.png"
-            width="550"
-          />
+      <Image
+        alt="Hero"
+        className="mx-auto aspect-video overflow-hidden dark:hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square"
+        height="550"
+        src="/logo.png"
+        width="550"
+      />
+      <Image
+        alt="Hero"
+        className="mx-auto aspect-video hidden dark:block overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square"
+        height="550"
+        src="/logo-white.png"
+        width="550"
+      />
     </>
-  )
-}
+  );
+};
 
-export default HeroImage
+export default HeroImage;
