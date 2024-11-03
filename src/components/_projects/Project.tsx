74074eb2aab98595593a959b30fed78f6fb872b3
Tomdieu <ivantomdio@gmail.com>
2024-11-03 21:28:20 +0100

    feat: enhance Project component with date display and improved layout; integrate BlurFade for animated project presentation

--- src/components/_projects/Project.tsx
+++ src/components/_projects/Project.tsx
@@ -22,59 +22,66 @@ type Props = {
 };
 
 const Project = ({ project, className }: Props) => {
-  const { id, title, description, image, tags, visit, source } = project;
+  const { id, title, description, image, tags, visit, source ,dates} = project;
   return (
-    <Card className={cn("max-w-lg dark:bg-slate-600 hover:scale-105 hover:shadow-lg hover:rotate-1 ease-linear transition-all", className)}>
-      <div className="w-full">
+    <Card className={cn("flex flex-col overflow-hidden border hover:shadow-lg transition-all duration-300 ease-out h-full", className)}>
+      <Link href={visit||source||"#"} className={cn("block cursor-pointer w-full")}>
           {image && (
             
 
             <Image
               src={image}
               width={500}
-              height={Math.floor((500 * 9) / 16)}
+              height={300}
               alt={title}
+              className="h-40 w-full overflow-hidden object-cover object-top"
             />
           )}
-        </div>
-      <CardHeader>
-        
-        <CardTitle>{title}</CardTitle>
-        <CardDescription className="line-clamp-2">
-          {description}
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div>
-          <h1 className="text-base font-bold">Stack</h1>
-          <div className="flex items-center space-x-2 flex-nowrap line-clamp-1">
-            {tags?.map((tag, index) => (
-              <Badge
-                key={index}
-                className="text-sm flex items-center space-x-2 space-y-2 gap-2 line-clamp-1"
-                variant="outline"
-              >
-                {/* {tag.icon} */}
-                {tag.label}
-              </Badge>
-            ))}
-          </div>
-        </div>
-        
-      </CardContent>
-      <CardFooter>
-      <div className={"flex items-center space-x-2 gap-1 px-2"}>
-          <Button asChild className="rounded-2xl">
-            <Link href={source!}>
-              <GitHubLogoIcon />
-              Github
-            </Link>
-          </Button>
-        </div>
-      </CardFooter>
-      
+        </Link>
+        <CardHeader className="px-2">
+            <div className="space-y-1">
+
+                <CardTitle className="mt-1 text-base">{title}</CardTitle>
+                {dates && (
+                    <time className="font-sans text-xs">{dates}</time>
+
+                )}
+                <CardDescription className="line-clamp-2">
+                {description}
+                </CardDescription>
+            </div>
+        </CardHeader>
+        <CardContent className="mt-auto flex flex-col px-2">
+            {tags && tags.length > 0 && (
+                <div className="mt-2 flex flex-wrap gap-1">
+                    {tags?.map((tag) => (
+                        <Badge
+                            className="px-1 py-0 text-[10px]"
+                            variant="secondary"
+                            key={tag.label}
+                        >
+                            {tag.label}
+                        </Badge>
+                    ))}
+                </div>
+            )}
+        </CardContent>
+        <CardFooter className="px-2 pb-2">
+            <div className="flex flex-row flex-wrap items-start gap-1">
+                {source && (
+                    <Link href={source}  target="_blank">
+                        <Badge className="flex gap-2 px-2 py-1 text-[10px]">
+                            <GitHubLogoIcon/>
+                            Github
+                        </Badge>
+                    </Link>
+                )}
+            </div>
+
+        </CardFooter>
+
     </Card>
-  );
+);
 };
 
 export default Project;
--- src/components/_projects/Projects.tsx
+++ src/components/_projects/Projects.tsx
@@ -2,22 +2,44 @@ import React from "react";
 import SectionDivider from "../global/SectionDivider";
 import { projectsData } from "@/constants/projects";
 import Project from "./Project";
+import BlurFade from "../magicui/blur-fade";
 
 type Props = {};
+
+const BLUR_FADE_DELAY = 0.04;
+
 
 const Projects = (props: Props) => {
   return (
     <section
       id="projects"
-      className="container mx-auto w-full py-6 sm:py-12 md:py-18 text-stone-900 dark:text-stone-50"
+      className={"font-poppins"}
+    // className="mx-auto w-full py-6 sm:py-12 md:py-18 text-stone-900 dark:text-stone-50"
     >
-      <SectionDivider />
-      <div>
-        <h1 className="text-xl lg:text-2xl font-bold">Projects</h1>
+      <div className="space-y-12 w-full py-12">
+        <BlurFade delay={BLUR_FADE_DELAY * 11}>
+
+          <div className={"space-y-2"}>
+            <h4 className="text-xl lg:text-2xl">My Projects</h4>
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Check out my latest work</h2>
+            <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+              I&apos;ve worked on a variety of projects, from simple
+              websites to complex web applications. Here are a few of my
+              favorites.
+            </p>
+          </div>
+        </BlurFade>
+        <div className="grid grid-cols-1 gap-3 lg:gap-2 sm:grid-cols-2 lg:grid-cols-3 mx-auto">
+          {projectsData.map((project, index) => (
+            <BlurFade
+              key={project.title}
+              delay={BLUR_FADE_DELAY * 12 + index * 0.05}
+            >
+              <Project key={index} project={project} className="w-[400px]" />
+            </BlurFade>))}
+        </div>
       </div>
-      <div className="mt-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 space-x-2 gap-2">
-            {projectsData.map((project, index) => (<Project key={index} project={project} className="w-[400px]"  />))}
-      </div>
+
     </section>
   );
 };
