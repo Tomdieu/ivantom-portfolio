2ff51826eab48de365d7b9f5f03e1b24e22f7c46
Tomdieu <ivantomdio@gmail.com>
2024-11-03 21:29:41 +0100

    feat: add ExperienceType definition for type safety in experience-related components

---
+++ types/index.d.ts
@@ -0,0 +1,10 @@
+declare type ExperienceType = {
+    logoUrl: string;
+    altText: string;
+    title: string;
+    subtitle?: string;
+    href?: string;
+    badges?: readonly string[];
+    period: string;
+    description?: string;
+}
