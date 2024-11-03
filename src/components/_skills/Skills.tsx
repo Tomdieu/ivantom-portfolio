ed2c87eb4ea176cdd0b1090eae3fb0bb966cc92f
Tomdieu <ivantomdio@gmail.com>
2024-11-03 21:29:18 +0100

    feat: enhance Skills component with animated skill badges; integrate BlurFade for improved presentation

--- src/components/_skills/Skills.tsx
+++ src/components/_skills/Skills.tsx
@@ -1,25 +1,53 @@
 import React from 'react'
-import SectionDivider from '../global/SectionDivider'
+import { Badge } from "@/components/ui/badge";
+import BlurFade from '../magicui/blur-fade';
+
+const BLUR_FADE_DELAY = 0.04;
+
 
 type Props = {}
 
 const Skills = (props: Props) => {
-  return (
-    <section id="skills" className="container mx-auto w-full py-6 px-6">
-      <SectionDivider/>
-        <div>
-            <h1 className="text-xl lg:text-2xl font-bold">Skills</h1>
-        </div>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-4'> 
-          <div>
-            <h1>Languages</h1>
-            <div>
-              <h1>Python</h1>
+    const skills = [
+        "React",
+        "Next.js",
+        "Typescript",
+        "Javascript",
+        "Node.js",
+        "Python",
+        "Postgres",
+        "Docker",
+        "Kubernetes",
+        "Java",
+        "Django Rest Framework",
+        "Ontology Web Language",
+        "Prisma",
+        "Tailwindcss",
+        "React Native",
+        "Expo",
+        "Sequelize",
+        "Websockets",
+        "Uml"
+    ]
+    return (
+        <section id="skills" className={"font-poppins"}>
+            {/*<SectionDivider/>*/}
+            <div className="flex min-h-0 flex-col gap-y-3">
+                <BlurFade delay={BLUR_FADE_DELAY * 9}>
+                    <h2 className="text-xl font-bold">Skills</h2>
+                </BlurFade>
+
+                <div className="flex flex-wrap gap-1">
+                    {skills.map((skill, id) => (
+                        <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
+
+                            <Badge key={skill}>{skill}</Badge>
+                        </BlurFade>
+                    ))}
+                </div>
             </div>
-          </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Skills
