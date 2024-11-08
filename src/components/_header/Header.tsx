2f634a5994d34a6ba33475f88b6f760639af2fd2
Tomdieu <ivantomdio@gmail.com>
2024-11-08 16:50:42 +0100

    fix: update navigation links in Header component for improved accuracy

--- src/components/_header/Header.tsx
+++ src/components/_header/Header.tsx
@@ -13,14 +13,14 @@ type Props = {
 const Header = ({className}: Props) => {
   const scrolled = useScrollTop()
   return (
-    <header className={cn(className,'container mx-auto px-5 md:px-0 flex items-center justify-between w-full py-5 sticky top-0 z-[50]',scrolled && "border-b")}>
+    <header className={cn(className,'flex items-center justify-between w-full py-5 sticky top-0 left-0 right-0 z-[50]',scrolled && "border-b")}>
        <Link className="flex items-center justify-center cursor-pointer" href="#">
        <h1 className='text-xl font-bold'>Tomdieu Ivan</h1>
 
        </Link>
         <nav className="ml-auto gap-4 sm:gap-6 items-center hidden sm:flex">
             <Link href={"#projects"} className="text-sm font-medium hover:underline underline-offset-4">Projects</Link>
-            <Link href={"#technology"} className="text-sm font-medium hover:underline underline-offset-4">Technology</Link>
+            <Link href={"#skills"} className="text-sm font-medium hover:underline underline-offset-4">Skills</Link>
             <Link href={"#about"} className="text-sm font-medium hover:underline underline-offset-4">About</Link>
           <ModeToggle/>
 
