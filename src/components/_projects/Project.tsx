f50455d096f6efdb5dbb0935ca965d69318231dd
Tomdieu <ivantomdio@gmail.com>
2024-11-08 16:50:47 +0100

    feat: enhance Project component with additional links and improve layout in Projects component

--- src/components/_projects/Project.tsx
+++ src/components/_projects/Project.tsx
@@ -22,66 +22,79 @@ type Props = {
 };
 
 const Project = ({ project, className }: Props) => {
-  const { id, title, description, image, tags, visit, source ,dates} = project;
+  const { id, title, description, image, tags, visit, source, dates, links } =
+    project;
   return (
-    <Card className={cn("flex flex-col overflow-hidden border hover:shadow-lg transition-all duration-300 ease-out h-full", className)}>
-      <Link href={visit||source||"#"} className={cn("block cursor-pointer w-full")}>
-          {image && (
-            
-
-            <Image
-              src={image}
-              width={500}
-              height={300}
-              alt={title}
-              className="h-40 w-full overflow-hidden object-cover object-top"
-            />
+    <Card
+      className={cn(
+        "flex flex-col border hover:shadow-lg transition-all duration-300 ease-out h-full w-full",
+        className
+      )}
+    >
+      <Link
+        href={visit || source || "#"}
+        className={cn("block cursor-pointer w-full")}
+      >
+        {image && (
+          <Image
+            src={image}
+            width={500}
+            height={300}
+            alt={title}
+            className="h-40 w-full overflow-hidden object-cover object-top rounded-t-md"
+          />
+        )}
+      </Link>
+      <CardHeader className="px-2">
+        <div className="space-y-1">
+          <CardTitle className="mt-1 text-base">{title}</CardTitle>
+          {dates && <time className="font-sans text-xs">{dates}</time>}
+          <CardDescription className="line-clamp-2">
+            {description}
+          </CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent className="mt-auto flex flex-col px-2">
+        {tags && tags.length > 0 && (
+          <div className="mt-2 flex flex-wrap gap-1">
+            {tags?.map((tag) => (
+              <Badge
+                className="px-1 py-0 text-[10px]"
+                variant="secondary"
+                key={tag.label}
+              >
+                {tag.label}
+              </Badge>
+            ))}
+          </div>
+        )}
+      </CardContent>
+      <CardFooter className="px-2 pb-2">
+        <div className="flex flex-row flex-wrap items-start gap-1">
+          {/* {source && (
+            <Link href={source} target="_blank">
+              <Badge className="flex gap-2 px-2 py-1 text-[10px]">
+                <GitHubLogoIcon />
+                Github
+              </Badge>
+            </Link>
+          )} */}
+          {links && links.length > 0 && (
+            <>
+              {links.map(({ href, label, icon }, index) => (
+                <Link href={href} key={index} target="_blank">
+                  <Badge className="flex gap-2 px-2 py-1 text-[10px]">
+                    {icon && icon}
+                    {label}
+                  </Badge>
+                </Link>
+              ))}
+            </>
           )}
-        </Link>
-        <CardHeader className="px-2">
-            <div className="space-y-1">
-
-                <CardTitle className="mt-1 text-base">{title}</CardTitle>
-                {dates && (
-                    <time className="font-sans text-xs">{dates}</time>
-
-                )}
-                <CardDescription className="line-clamp-2">
-                {description}
-                </CardDescription>
-            </div>
-        </CardHeader>
-        <CardContent className="mt-auto flex flex-col px-2">
-            {tags && tags.length > 0 && (
-                <div className="mt-2 flex flex-wrap gap-1">
-                    {tags?.map((tag) => (
-                        <Badge
-                            className="px-1 py-0 text-[10px]"
-                            variant="secondary"
-                            key={tag.label}
-                        >
-                            {tag.label}
-                        </Badge>
-                    ))}
-                </div>
-            )}
-        </CardContent>
-        <CardFooter className="px-2 pb-2">
-            <div className="flex flex-row flex-wrap items-start gap-1">
-                {source && (
-                    <Link href={source}  target="_blank">
-                        <Badge className="flex gap-2 px-2 py-1 text-[10px]">
-                            <GitHubLogoIcon/>
-                            Github
-                        </Badge>
-                    </Link>
-                )}
-            </div>
-
-        </CardFooter>
-
+        </div>
+      </CardFooter>
     </Card>
-);
+  );
 };
 
 export default Project;
--- src/components/_projects/Projects.tsx
+++ src/components/_projects/Projects.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import SectionDivider from "../global/SectionDivider";
 import { projectsData } from "@/constants/projects";
 import Project from "./Project";
 import BlurFade from "../magicui/blur-fade";
@@ -16,11 +15,11 @@ const Projects = (props: Props) => {
       className={"font-poppins"}
     // className="mx-auto w-full py-6 sm:py-12 md:py-18 text-stone-900 dark:text-stone-50"
     >
-      <div className="space-y-12 w-full py-12">
+      <div className="space-y-12 py-12">
         <BlurFade delay={BLUR_FADE_DELAY * 11}>
 
           <div className={"space-y-2"}>
-            <h4 className="text-xl lg:text-2xl">My Projects</h4>
+            <span className="text-xl lg:text-2xl font-bold">My Projects</span>
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Check out my latest work</h2>
             <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
               I&apos;ve worked on a variety of projects, from simple
@@ -29,13 +28,13 @@ const Projects = (props: Props) => {
             </p>
           </div>
         </BlurFade>
-        <div className="grid grid-cols-1 gap-3 lg:gap-2 sm:grid-cols-2 lg:grid-cols-3 mx-auto">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3 w-full">
           {projectsData.map((project, index) => (
             <BlurFade
               key={project.title}
               delay={BLUR_FADE_DELAY * 12 + index * 0.05}
             >
-              <Project key={index} project={project} className="w-[400px]" />
+              <Project key={index} project={project} />
             </BlurFade>))}
         </div>
       </div>
