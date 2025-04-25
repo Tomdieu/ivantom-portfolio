bba219ff6fdd2ec7bb2341d97281dcdbb6cf1b40
Tomdieu <ivantomdio@gmail.com>
2025-04-25 10:30:20 +0100

    fix: adjust sitemap priority to improve search engine indexing

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,8 +2,9 @@ module.exports = {
   siteUrl: "https://ivantomdieu.vercel.app/",
   generateRobotsTxt: true,
   changefreq: "daily",
-  priority: 1,
+  priority: 0.7,
   sitemapSize: 5000,
+  // exclude: ['/secret-page'],
   robotsTxtOptions: {
     policies: [
       {
