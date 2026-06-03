bfaf782d54dc45cd860d5f90601a35bb6defb8a7
tomdieu <ivantomdio@gmail.com>
2026-06-03 08:17:55 +0100

    chore(sitemap): simplify sitemap configuration

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,15 +1,8 @@
 module.exports = {
   siteUrl: "https://ivantomdieu.vercel.app",
   generateRobotsTxt: true,
-  changefreq: "daily",
-  priority: 1.0,
-  lastmod: true,
-  sitemapSize: 5000,
   outDir: "public",
   robotsTxtOptions: {
-    additionalSitemaps: [
-      'https://ivantomdieu.vercel.app/sitemap.xml',
-    ],
     policies: [
       {
         userAgent: "*",
